b8a2928d8d7dcb39f6771e47f520c07b7aae1ac0
Ankitnath <ankitnath1583@gmail.com>
2026-03-29 23:29:26 +0530

    Add files via upload

---
+++ day53.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+struct Node* newNode(int data) {
+    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+    node->data = data;
+    node->left = node->right = NULL;
+    return node;
+}
+
+
+void mirror(struct Node* root) {
+    if (root == NULL)
+        return;
+
+    struct Node* temp = root->left;
+    root->left = root->right;
+    root->right = temp;
+
+    mirror(root->left);
+    mirror(root->right);
+}
+
+void inorder(struct Node* root) {
+    if (root == NULL)
+        return;
+
+    inorder(root->left);
+    printf("%d ", root->data);
+    inorder(root->right);
+}
+int main() {
+    struct Node* root = newNode(1);
+    root->left = newNode(2);
+    root->right = newNode(3);
+    root->left->left = newNode(4);
+    root->left->right = newNode(5);
+    root->right->left = newNode(6);
+    root->right->right = newNode(7);
+
+    // Convert to mirror
+    mirror(root);
+
+    // Print inorder of mirrored tree
+    inorder(root);
+
+    return 0;
+}
